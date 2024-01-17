438b54b76e046b442b0a578543cacf986d207e0a
MeRajaKumar <teamaayushgupta@gmail.com>
2024-01-17 22:16:26 +0530

    all changes

---
+++ DataTypeEG.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class DataTypeEG {
+     //Que >> Example of data types?? ⬇️👍
+     float b=23.56f;
+     char c='x';
+     double d=43.65d;
+     long l=1234;
+     byte byt=23;
+     short sht=1234;
+     String str="rouank singh";
+     Scanner ob=new Scanner(System.in);
+     System.out.print("Enter Int value:-");
+     a=ob.nextInt();
+     System.out.print("Enter Float value:-");
+     b=ob.nextFloat();
+     System.out.print("Enter double:-");
+     d=ob.nextDouble();
+     System.out.print("Enter long value:-");
+     l=ob.nextLong();
+     System.out.print("Enter byte value:-");
+     byt=ob.nextByte();
+     System.out.print("Enter short value:-");
+     sht=ob.nextShort();
+     System.out.print("Enter String:-");
+     str=ob.nextLine();
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(c);
+     System.out.println(d);
+     System.out.println(l);
+     System.out.println(byt);
+     System.out.println(sht);
+     System.out.println(str);
+     int a=1;
+}
