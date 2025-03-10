b871f106fcaef7948f125c03e3d2e07c77026288
Raja Kumar <teamaayushgupta@gmail.com>
2025-03-10 18:15:00 +0530

    two code added

---
+++ 10mar-maxoftwo.java
@@ -0,0 +1,20 @@
+// Find Maximum of Two Numbers
+
+import java.util.Scanner;
+public class MaxofTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st num: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter 2nd num: ");
+        int num2 = sc.nextInt();
+
+        if(num1 > num2 ){
+            System.out.println(num1 +" is greater than "+num2);
+        }else{
+            System.out.println(num2 +" is greater than "+num1);
+        }
+
+    }
+}
---
+++ 10march-swaptwonum.java
@@ -0,0 +1,16 @@
+// Swap Two Numbers Without Using Third Variable
+import java.util.Scanner;
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+
+        System.out.println("Before Swapping: a = " + a + ", b = " + b);
+
+        // Swapping without third variable
+        a = a + b;  // a becomes 15 (5 + 10)
+        b = a - b;  // b becomes 5 (15 - 10)
+        a = a - b;  // a becomes 10 (15 - 5)
+
+        System.out.println("After Swapping: a = " + a + ", b = " + b);
+    }
+}
