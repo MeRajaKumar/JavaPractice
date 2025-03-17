a7e98669416f297c780d6617cbc2892de0888acb
Raja Kumar <teamaayushgupta@gmail.com>
2025-03-17 13:32:32 +0530

    updated and initial commit

--- 11mar-maxofthree.java
+++ 11mar-maxofthree.java
@@ -1,6 +1,6 @@
 // Find Maximum of Three Numbers
 import java.util.Scanner;
-public class Maxofthree {
+public class MinMaxThreeNumber {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -11,14 +11,32 @@ public class Maxofthree {
         System.out.print("Enter 3rd number for check: ");
         float num3 = sc.nextInt();
 
-        if(num1 > num2) {
-            if(num1 > num3){
-                System.out.print(num1+ " is maximum!");
-            }else{
-                System.out.print(num3+ " is maximum!");
-            }
-        }else{
-                System.out.print(num2+" is maximum!");
-            }
+        // finding maximum
+        int max = Math.max(num1, Math.max(num2,num3));
+        // finding minimum
+        int min = Math.min(num1, Math.min(num2,num3));
+
+        System.out.print("Maximum number : "+max);
+        System.out.print("Minimum number : "+min);
+
+        // method 2 :
+        int max1, min1;
+        if (num1 >= num2 && num1 >= num3) {
+            max = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            max = num2;
+        } else {
+            max = num3;
+        }
+
+        if (num1 <= num2 && num1 <= num3) {
+            min = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            min = num2;
+        } else {
+            min = num3;
+        }
+
+        scanner.close();
     }
 }
--- 11mar-primeno.java
+++ 11mar-primeno.java
@@ -1,11 +1,16 @@
-// Check if a Number is Prime
+// Check if a Number is Prime eg: 1,2,3,5,7
 import java.util.Scanner;
 public class Maxofthree {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
 
-
+        if (num > 2){
+            if (num  )
+        }else{  
+            System.out.print("Please enter the number grater than 2! or it is not prime num")
+        }
     }
 }
