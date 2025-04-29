78dfd35f31ef99102a93986198c3e675d92290bd
Raja Kumar <teamaayushgupta@gmail.com>
2025-04-29 20:15:31 +0530

    initial commit

---
+++ GFG.java
@@ -0,0 +1,25 @@
+class GFG {
+    public static void revStg(String Param1) {
+        String rev = "";
+        for (int i = Param1.length() - 1; i >= 0; i--) {
+            rev = rev + Param1.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+    public static void main(String args[]) {
+        revStg("Zopsmart is hiring!");
+
+    // String Param1="Zop kumar";
+    // String rev="";
+    // for(int i=Param1.length()-1;i>=0;i--){
+    //     rev=rev+Param1.charAt(i);
+    // };
+    // System.out.println(rev);
+
+    }
+}
+
+
+    
+
---
+++ ReverseStg.java
@@ -0,0 +1,13 @@
+class ReverseStg {
+    public static void revStg(String Param1) {
+        String rev = "";
+        for (int i = Param1.length() - 1; i >= 0; i--) {
+            rev = rev + Param1.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+    public static void main(String args[]) {
+        revStg("ram is here"); 
+    }
+}
