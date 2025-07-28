a53bd6cd16a0b89abf79328e897964709fa5211d
Raja Kumar <teamaayushgupta@gmail.com>
2025-07-28 23:39:46 +0530

    initiated new file

---
+++ 100 java practice programs/EvenNum1to100.java
@@ -0,0 +1,17 @@
+// question: Print even numbers from 1 to 100
+// class EvenNum1to100 {
+//     public static void main(String[] args) {
+//         for (int i = 2; i <= 100; i += 2) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// question: Print odd numbers from 1 to 100
+class PrintOddNum1to100 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ 100 java practice programs/Print1to100Num.java
@@ -0,0 +1,8 @@
+// question: Print numbers from 1 to 100
+class Print1to100Num {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
